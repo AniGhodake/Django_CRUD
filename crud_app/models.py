259f255d00b68a3d6b9d56755d73d799cfbaefc7
Aniket_Ghodake <ghodakeaniket27@gmail.com>
2025-10-05 21:18:01 +0530

    models

--- crud_app/models.py
+++ crud_app/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
 # Create your models here.
+class Student(models.Model):
+    Gender_choices = [
+        ('M', 'Male'),
+        ('F', 'Female'),
+        ('O', 'Other'),
+    ]
+
+    name = models.CharField(max_length = 100)
+    email = models.EmailField()
+    course = models.CharField(max_length = 100)
+
+    gender = models.CharField(max_length = 1, choices = Gender_choices, default = 'M')
+
+
+    hobbies = models.CharField(max_length = 255, blank = True)
+
+    is_active = models.BooleanField(default= True)
+
+
+
+    def get_hobby_list(self):
+        """ Return hobbies as Python list for display/edit. """
+        return self.hobbies.split(',') if self.hobbies else []
+    
+
+    def __str__(self):
+        return self.name
